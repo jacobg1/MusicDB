ca4c56a99396f9f5179310618f943dfbee59fbb4
Jacob Greenwald <jeg4t@virginia.edu>
2018-06-30 17:02:10 -0400

    rebuild

---
+++ api/controllers/SongController.js
@@ -0,0 +1,12 @@
+/**
+ * SongController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
